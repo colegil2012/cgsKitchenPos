e84f102626fe8c1987b4175a22d51326bd7c755e
Christopher Gilbert <colegil2012@gmail.com>
2026-06-10 01:28:23 -0400

    another update fix

--- update.sh
+++ update.sh
@@ -16,8 +16,8 @@
 
 set -e
 
-APP_DIR="/home/druid-mobile/celtech-pos"
-LOG_FILE="/home/druid-mobile/update.log"
+APP_DIR="/home/druid-pos/cgsKitchenPos"
+LOG_FILE="/home/druid-pos/update.log"
 ENV_FILE="/etc/celtech/env"
 ENV_OUT="$APP_DIR/.env"
 BRANCH="main"
